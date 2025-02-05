a35f6010be080abbac7304e55d0804ce31f4f742
rsveinson <rsveinson@mts.net>
2025-02-05 12:57:27 -0600

    removed extends from Gp.java

--- GP.java
+++ GP.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
  * @author 
 */
 
-public class GP extends Doctor{
+public class GP{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
